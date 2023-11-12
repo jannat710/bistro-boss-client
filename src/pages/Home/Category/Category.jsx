38000914b3693404b092ba9b8ffef9b87ad6ec3c
e <jannatul0040@gmail.com>
2023-11-12 12:49:49 +0600

    added section title

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -8,10 +8,13 @@ import slide2 from '../../../assets/home/slide2.jpg'
 import slide3 from '../../../assets/home/slide3.jpg'
 import slide4 from '../../../assets/home/slide4.jpg'
 import slide5 from '../../../assets/home/slide5.jpg'
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 
 const Category = () => {
     return (
-        <Swiper
+<section>
+    <SectionTitle subHeading={'From 11.00am to 10.00pm'} heading={'Order Online'}></SectionTitle>
+<Swiper
             slidesPerView={4}
             spaceBetween={30}
             centeredSlides={true}
@@ -42,6 +45,7 @@ const Category = () => {
                 <h3 className='text-4xl uppercase text-center text-white -mt-16'>Salads</h3>
             </SwiperSlide>
         </Swiper>
+</section>
     );
 };
 
