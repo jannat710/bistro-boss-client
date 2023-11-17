8b180d0c37d967d6d52be9e63b05b5cd6a1b76b9
e <jannatul0040@gmail.com>
2023-11-17 22:21:58 +0600

    cart icon setup

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -2,6 +2,9 @@
 
 const FoodCard = ({item}) => {
     const {name, image, price, recipe} = item;
+    const handleAddToCart = food => {
+console.log(food)
+    }
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
         <figure><img src={image} /></figure>
@@ -10,8 +13,7 @@ const FoodCard = ({item}) => {
             <h2 className="card-title">{name}</h2>
             <p>{recipe}</p>
             <div className="card-actions justify-end">
-                <button
-                    
+                <button onClick={()=>handleAddToCart(item)}                   
                     className="btn btn-outline bg-slate-100 border-0 border-b-4 border-[#BB8506] mt-4 hover:text-[#BB8506]"
                 >Add to Cart</button>
             </div>
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import cart from '../../../assets/icon/cart.png'
 import { useContext } from "react";
 import { AuthContext } from "../../../providers/AuthProvider";
+import { FaShoppingCart } from 'react-icons/fa';
 
 
 const Navbar = () => {
@@ -13,24 +14,33 @@ const Navbar = () => {
     }
 
     const navlink = <div className="flex flex-col  md:flex-row lg:flex-row justify-center items-center">
-    <li><Link className="font-semibold text-base"  to="/">Home</Link> </li>
-    <li><Link className="font-semibold text-base" to="/contact">Contact us</Link> </li>
-    <li><Link className="font-semibold text-base" to="/dashboard">Dashboard</Link> </li>
-    <li><Link className="font-semibold text-base" to="/menu">Our Menu</Link> </li>
-    <li><Link className="font-semibold text-base" to="/order/salad">Order Food</Link> </li>
-    <li><Link className="font-semibold text-base" to="/secret">Secret</Link> </li>
-    <li><img className="h-14 w-20" src={cart} alt="" /> </li>
+        <li><Link className="font-semibold text-base" to="/">Home</Link> </li>
+        <li><Link className="font-semibold text-base" to="/contact">Contact us</Link> </li>
+        <li><Link className="font-semibold text-base" to="/dashboard">Dashboard</Link> </li>
+        <li><Link className="font-semibold text-base" to="/menu">Our Menu</Link> </li>
+        <li><Link className="font-semibold text-base" to="/order/salad">Order Food</Link> </li>
+        <li><Link className="font-semibold text-base" to="/secret">Secret</Link> </li>
+        <li><img className="h-14 w-20" src={cart} alt="" /> </li>
+        <li>
+            <Link to='/'>
+                <button className="btn">
+                <FaShoppingCart className="mr-2"></FaShoppingCart>
+                    <div className="badge badge-secondary">+0</div>
+                </button>
+            </Link>
+        </li>
 
-    {
+        {
             user ? <>
-            <span>{user?.displayName}</span>
+                {/* <span>{user?.displayName}</span>
+            <img className="h-6 w-6 rounded-full" src={user?.photoURL} alt="" /> */}
                 <button onClick={handleLogOut} className="btn btn-ghost">LogOut</button>
             </> : <>
-            <li><Link className="font-semibold text-base" to="/login">Login</Link> </li>
+                <li><Link className="font-semibold text-base" to="/login">Login</Link> </li>
             </>
         }
 
-        
+
     </div>
     return (
         <div className="navbar fixed z-10 bg-opacity-30 bg-black text-white max-w-screen-xl mx-auto">
@@ -44,8 +54,8 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="flex flex-col">
-                <p>BISTRO BOSS</p>
-                <p className="uppercase">Restaurant</p>
+                    <p>BISTRO BOSS</p>
+                    <p className="uppercase">Restaurant</p>
                 </div>
             </div>
             <div className="navbar-center hidden lg:flex">
@@ -53,9 +63,9 @@ const Navbar = () => {
                     {navlink}
                 </ul>
             </div>
-                <div className="navbar-end">
-                    <button className="">Sign out</button>
-                </div>
+            <div className="navbar-end">
+                <button className="">Sign out</button>
+            </div>
         </div>
     );
 };
