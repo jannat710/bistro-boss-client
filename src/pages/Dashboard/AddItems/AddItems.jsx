0cdf84dba816ee61784ca01df9fce7bdc220a057
e <jannatul0040@gmail.com>
2023-11-23 20:05:31 +0600

    Upload image to image hosting server imgbb and get image url

--- src/pages/Dashboard/AddItems/AddItems.jsx
+++ src/pages/Dashboard/AddItems/AddItems.jsx
@@ -1,25 +1,54 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { FaUtensils } from "react-icons/fa";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 const AddItems = () => {
-    const { register, handleSubmit } = useForm()
-    const onSubmit = (data) => {
+    const { register, handleSubmit } = useForm();
+    const axiosPublic = useAxiosPublic();
+    const axiosSecure = useAxiosSecure();
+    const onSubmit =async (data) => {
         console.log(data)
+        // image upload to imgbb and then get an url
+        const imageFile = { image: data.image[0] }
+        const res = await axiosPublic.post(image_hosting_api, imageFile, {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        });
+        console.log(res.data)
 
     };
 
     return (
         <div className="">
-            <div>
+        
                 <SectionTitle heading='ADD AN ITEM' subHeading="What's new?"></SectionTitle>
-
-            </div>
-            <div>
+                <div>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <input {...register("name")} />
-
-                    <select defaultValue="default" {...register('category', { required: true })}
+                    <div className="form-control w-full my-6">
+                        <label className="label">
+                            <span className="label-text">Recipe Name*</span>
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="Recipe Name"
+                            {...register('name', { required: true })}
+                            required
+                            className="input input-bordered w-full" />
+                    </div>
+                    <div className="flex gap-6">
+                        {/* category */}
+                        <div className="form-control w-full my-6">
+                            <label className="label">
+                                <span className="label-text">Category*</span>
+                            </label>
+                            <select defaultValue="default" {...register('category', { required: true })}
                                 className="select select-bordered w-full">
                                 <option disabled value="default">Select a category</option>
                                 <option value="salad">Salad</option>
@@ -28,9 +57,39 @@ const AddItems = () => {
                                 <option value="dessert">Dessert</option>
                                 <option value="drinks">Drinks</option>
                             </select>
-                    <input type="submit" />
+                        </div>
+
+                        {/* price */}
+                        <div className="form-control w-full my-6">
+                            <label className="label">
+                                <span className="label-text">Price*</span>
+                            </label>
+                            <input
+                                type="number"
+                                placeholder="Price"
+                                {...register('price', { required: true })}
+                                className="input input-bordered w-full" />
+                        </div>
+
+                    </div>
+                    {/* recipe details */}
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Recipe Details</span>
+                        </label>
+                        <textarea {...register('recipe')} className="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
+                    </div>
+
+                    <div className="form-control w-full my-6">
+                        <input {...register('image', { required: true })} type="file" className="file-input w-full max-w-xs" />
+                    </div>
+
+                    <button className="btn">
+                        Add Item <FaUtensils className="ml-4"></FaUtensils>
+                    </button>
                 </form>
             </div>
+
         </div>
     );
 };
